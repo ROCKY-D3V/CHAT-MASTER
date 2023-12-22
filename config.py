c427081e6edd6a461c499bd716c7839227b83671
ʀσƈƙყ <146221888+ROCKY-D3V@users.noreply.github.com>
2023-12-22 08:52:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,9 +9,9 @@ API_HASH = getenv("API_HASH", "cf2a75861140ceb746c7796e07cbde9e")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 OWNER_ID = int(getenv("OWNER_ID", 6562419243)
 MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", ")
-UPDATE_CHNL = getenv("UPDATE_CHNL", "DadEyeBotz")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "Dev_Arora_0981")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "DECENT_CHATTING")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "XD_N3TWORK")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "M3_4_U")
 
 # Random Start Images
 IMG = [
